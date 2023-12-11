3377fa6bd6a44c80feb39195038dde0eef3518be
nbaier <nico.baier@thyssenkruppp-automotive.com>
2023-12-11 21:08:47 +0100

    If you ever been curious

---
+++ variables.py
@@ -0,0 +1,15 @@
+name =input("name")
+age = input(str("age"))
+is_new = False
+
+print(str(name) + ", " + str(age) + " years old")
+if is_new != True:
+    pass
+else:
+    print("new patient")
+#################################################################################
+name=input("What's your name? My name is ")
+age=input("How old are you? I am....years old.")
+color=input("What is your favorite color?")
+print(str(name)+" is "+str(age)+" years old and his favorite color is ")
+print (color+".")
