b420ad1eafd3d019e6cde632a6acdef4ffebd98e
nbaier <nico.baier@thyssenkruppp-automotive.com>
2023-12-21 08:54:11 +0100

    part1

---
+++ Advent of Code/2016/day1.py
@@ -0,0 +1,22 @@
+import math
+
+
+def read_input(input_string):
+    elements: list = input_string.split(", ")
+    calc_dist(elements)
+
+
+def calc_dist(elements):
+    sight = "N"
+    x = 0
+    y = 0
+    counter = True
+
+
+
+def main():
+    read_input(str(input("Wie ist die Zeichenfolge? \n")))
+
+
+if __name__ == "__main__":
+    main()
