560457f708332a00de55b16eb854e97772293a38
nbaier <nico.baier@thyssenkruppp-automotive.com>
2023-12-31 16:25:29 +0100

    day 1 complete

--- Advent of Code/2016/day1.py
+++ Advent of Code/2016/day1.py
@@ -12,6 +12,68 @@ def calc_dist(elements):
     y = 0
     counter = True
 
+
+
+    for elements in elements:
+        direction = str(elements[0])
+        distance = int(elements[1:])
+        if counter:
+            if direction == "L":
+                sight = "W"
+                counter = False
+            else:
+                sight = "O"
+                counter = False
+
+        if sight == "N":
+            if direction == "R":
+                x = x + distance
+                sight = "O"
+                continue
+            if direction == "L":
+                x = x - distance
+                sight = "W"
+                continue
+        if sight == "O":
+            if direction == "R":
+                y = y - distance
+                sight = "S"
+                continue
+            if direction == "L":
+                y = y + distance
+                sight = "N"
+                continue
+
+        if sight == "S":
+            if direction == "R":
+                x = x - distance
+                sight = "W"
+                continue
+            if direction == "L":
+                x = x + distance
+                sight = "O"
+                continue
+
+        if sight == "W":
+            if direction == "R":
+                y = y + distance
+                sight = "N"
+                continue
+            if direction == "L":
+                y = y - distance
+                sight = "S"
+                continue
+    calc_blocks(x, y)
+
+
+def calc_blocks(x, y):
+    x = abs(x)
+    y = abs(y)
+
+    print(x + y)
+    pass
+
+
 def main():
     read_input(str(input("Wie ist die Zeichenfolge? \n")))
 
