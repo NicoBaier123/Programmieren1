ae8438c225965c6fb1940a0f5f7f12913ec60393
nbaier <nico.baier@thyssenkruppp-automotive.com>
2024-01-16 18:33:08 +0100

    aoc d2, list, tuple, dict

--- Unterricht/dictionary_uebung.py
+++ Unterricht/dictionary_uebung.py
@@ -1,55 +1,58 @@
 import re
 
-transactions = [
-    {'type': 'purchase', 'amount': 50, 'date': '2024-01-15'},
-    {'type': 'sale', 'amount': 30.5, 'date': '2024-01-15'}
-]
+transactions = [{'type': 'purchase', 'amount': 50, 'date': '2024-01-14'},
+                {'type': 'sale', 'amount': 30.5, 'date': '2024-01-15'}]
+
 transaction_type = transactions[0]['type']
 transaction_amount = transactions[0]['amount']
 transaction_date = transactions[0]['date']
 
 
-# using list comprehension
+# Mach eine Liste aus den transactions
 def list_of(my_key):
-    amount_values = [transaction['my_key'] for transaction in transactions]
-    print(list_of('amount'))
+    amount_values = [transaction['amount'] for transaction in transactions]
     return amount_values
 
 
+print(list_of('amount\n'))
+
+
+# Summe der transaktionen aus liste
 def sum_up(my_type):
     Summe = [transaction['amount'] for transaction in transactions
              if transaction['type'] == my_type]
     return (sum(Summe))
 
 
+# alle die an einem bestimmten Tag gemacht wurden
+def findall(my_key, datum):
+    trans_gestern = [transaction for transaction in transactions
+                     if transaction[my_key] == datum]
+    return trans_gestern
+
+
+def is_valid_data_format(date_strings):
+    date_pattern = re.compile(
+        r'^\d{4}-\d{2}-\d{2}$')  # ^ = Anfang \d = 4 bestimmte character dann  "-" dann \d = 2 bestimmte chars "-" \d wieder 2 bestimmte chars $Ende von String
+    # doestnt check for day/month order
+    return bool(date_pattern.match(date_strings))
+
+
+my_transactions = findall('date', '2024-01-14')
+print('\n', my_transactions)
+print(len(my_transactions), '\n')
+
 income = sum_up('purchase')
 expenses = sum_up('sale')
-print('income=', income)
-print('expenses=', expenses)
+print('income', income)
+print('expenses', expenses)
 
 if (income > expenses):
     print('You made money')
 else:
-    print('You lost money!')
+    print('you lost money')
 
-
-def find_all(my_key, my_value):
-    values = [transaction for transaction in transactions if
-              transaction[my_key] == my_value]
-    return (values)
-
-
-my_transactions = find_all('date', '2024-01-14')
-print(my_transactions)
-print(len(my_transactions))
-
-
-def is_valid_date_format(date_string):
-    date_pattern = re.compile(r'^\d{4}-\d{2}-\d{2}$')
-    return bool(date_pattern.match(date_string))
-
-#if len(my_transactions)>0:
-my_date = my_transactions[0]['date']
+my_date = my = my_transactions[0]['date']
 print(my_transactions[0]['date'])
 print(type(my_date))
-print(is_valid_date_format(my_date))
+print(is_valid_data_format(my_date))
